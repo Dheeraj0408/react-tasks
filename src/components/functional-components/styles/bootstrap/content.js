611e3aaddcfb3eca3ad9050646bfa752f3ef64ca
Dheeraj0408 <dheeraj4f0@gmail.com>
2024-02-19 21:49:28 +0530

    Task done on fancytext bootstrap

--- src/components/functional-components/styles/bootstrap/content.js
+++ src/components/functional-components/styles/bootstrap/content.js
@@ -31,9 +31,9 @@ export default Content;
 
 export const Card=()=>{
 return(
-  <div className="card" style={{ width: 400 }}>
+  <div className="card" style={{ width: 350, height:380 }}>
   {/* <img className="card-img-top" src="img_avatar1.png" alt="Card image" /> */}
-  {/* <ImageComponent/> */}
+  <ImageComponent/>
   <div className="card-body">
     <h4 className="card-title">John Doe</h4>
     <p className="card-text">Some example text.</p>
