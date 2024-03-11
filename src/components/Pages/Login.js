27b8e4930ac2ab68b91e83d9a65f9dd4e43afc93
Dheeraj0408 <dheeraj4f0@gmail.com>
2024-03-11 10:33:19 +0530

    styles added

--- src/components/Pages/Login.js
+++ src/components/Pages/Login.js
@@ -1,11 +1,53 @@
+import { useState } from "react";
 import Header from "../Navbar/header";
 
-const LoginPage=()=>{
-    return(
-        <>
-        <Header/>
-        <h1>Login Page</h1>
-        </>
-    )
-}
+const LoginPage = () => {
+    const[username,setUsername]=useState('');
+    const[usernameError,setUsernameError]=useState(null);
+    const[password,setPassword]=useState('');
+    const[passwordError,setPasswordError]=useState('');
+    const handleUsername=(event)=>{
+        let uname=event.target.value;
+        setUsername(event.target.value);
+        if(uname.length>7){
+            setUsernameError('The username should be less than 7 characters');
+        }
+        else{
+            setUsernameError(null);
+        }
+
+    }
+    const handlePassword=(event)=>{
+        setPassword(event.target.value);
+        if(password.length<10){
+            setPasswordError('The password must contains atleast 10 characters')
+        }
+        else{
+            setPasswordError(null);
+        }
+    }
+
+    const handleSubmit=()=>{
+
+    }
+  return (
+    <>
+      <Header />
+      <div style={{margin:"auto",border:"1px solid black", width:"fit-content",padding:"20px"}}>
+        <h1 style={{marginBottom:"30px"}}>Login Page</h1>
+        <form onSubmit={handleSubmit} style={{display:"flex",marginBottom:"30px",flexDirection:"column",rowGap:"10px"}}>
+          <div>
+          <input type="text" placeholder="Enter your username" onChange={(event)=>handleUsername(event)}/>
+            {usernameError?<p style={{color:"red"}}>{usernameError}</p>:null}
+          </div>
+           <div>
+           <input type="password" placeholder="Enter your password" onChange={(event)=>handlePassword(event)}/>
+            {passwordError?<p style={{color:'red'}}>{passwordError}</p>:null}
+           </div>
+            <button type="submit">Login</button>
+        </form>
+      </div>
+    </>
+  );
+};
 export default LoginPage;
--- src/components/Pages/home.js
+++ src/components/Pages/home.js
@@ -1,50 +1,66 @@
 import { useEffect, useState } from "react";
 import Header1 from "../Navbar/header1";
 import axios from "axios";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import CustomSpinner from "../functional-components/Spinners/spinner1";
+import { Link } from "react-router-dom";
+import "./homescreen.css"
 
 const HomePage = () => {
-  const[data,setData]=useState([]);
+  const [data, setData] = useState([]);
   useEffect(() => {
-    axios.get("https://fakestoreapi.com/products").then((response) => {
-      if (response.status === 200) {
-        setData(response.data);
-      }
-    }).catch(err=>console.log(err));
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((response) => {
+        if (response.status === 200) {
+          console.log(response);
+          setData(response.data);
+        }
+      })
+      .catch((err) => console.log(err));
   }, []);
   return (
     <>
       <Header1 />
-      {
-        data.length>0?data.map((eachProduct)=>{
-          return(<>
-          <CustomCard product={eachProduct}/></>)
-        })
-}
-      
+      <div className="cards-container">
+        {data.length > 0 ? (
+          data.map((eachProduct) => {
+            return (
+              <div key={eachProduct.id}>
+                <CustomCard product={eachProduct} />
+              </div>
+            );
+          })
+        ) : (
+          <CustomSpinner />
+        )}
+      </div>
     </>
   );
 };
 export default HomePage;
 
-export function CustomCard({data}) {
-  const {id,title,description,image,price}=data
+export function CustomCard(props) {
+  // const {
+  //   product: { title, description, image, price },
+  // } = props;
+  const { title, id, description, image, price, category } = props.product;
+  const handleClick = () => {};
   return (
     <>
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={image} />
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>
-         {description}
-        </Card.Text>
-        <Button variant="primary">View Product</Button>
-      </Card.Body>
-    </Card>
+      <Card className="custom-cards">
+        <Card.Img variant="top" src={image} width={250} height={250} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{category}</Card.Text>
+          <p style={{ color: "red" }}>₹{price}</p>
+          {/* <Button variant="primary">View Product</Button> */}
+          <button>
+            <Link to={`/${category}/${id}`} style={{textDecoration:"none",color:"black"}}>See Details</Link>
+          </button>
+        </Card.Body>
+      </Card>
     </>
   );
 }
-
-
